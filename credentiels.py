ec089157e4decacf8cdfcc7378ec7e0ca8091287
ahmedbmz <44393609+ahmedbmz@users.noreply.github.com>
2021-01-25 18:44:39 +0100

    Adding the project to git

---
+++ credentiels.py
@@ -0,0 +1,21 @@
+# MQTT Broker info
+MQTT_IP = '';
+MQTT_PORT = 1883
+
+# SQL credentials
+SQL_USER = '';
+SQL_PASSWORD = '';
+
+# User variables for MQTT Broker connection
+mqttBroker = "broker.emqx.io"
+mqttBrokerPort = 1883
+mqttUser = None
+mqttPassword = None
+topic = "/python/mqtt"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+# User variables for MQTT Broker connection
+mysqlHost = "bbzj937svfngzjrooibe-mysql.services.clever-cloud.com"
+mysqlUser = "uaxb8xwaqzqfhyg5"
+mysqlPassword = "TzBw8FtF0rB9hSVsPXye"
+dbName= "bbzj937svfngzjrooibe"
